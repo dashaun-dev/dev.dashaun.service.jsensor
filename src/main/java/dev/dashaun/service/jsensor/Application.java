534589b89cc062c9fb4526848147b8ad86bec304
DaShaun <dashaun@dashaun.com>
2023-07-10 07:39:06 -0600

    first pass

---
+++ src/main/java/dev/dashaun/service/jsensor/Application.java
@@ -0,0 +1,66 @@
+package dev.dashaun.service.jsensor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.Sensors;
+import oshi.software.os.OperatingSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController
+class SensorController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SensorController.class);
+
+	static List<String> oshi = new ArrayList<>();
+
+	@GetMapping("/")
+	public String index() {
+		SystemInfo si = new SystemInfo();
+
+		HardwareAbstractionLayer hal = si.getHardware();
+		OperatingSystem os = si.getOperatingSystem();
+
+		logger.info("Checking Processor...");
+		printProcessor(hal.getProcessor());
+
+		logger.info("Checking Sensors...");
+		printSensors(hal.getSensors());
+
+		StringBuilder output = new StringBuilder();
+		for (String line : oshi) {
+			output.append(line);
+			if (line != null && !line.endsWith("\n")) {
+				output.append('\n');
+			}
+		}
+		logger.info("Printing Operating System and Hardware Info:{}{}", '\n', output);
+		return output.toString();
+	}
+
+	private static void printProcessor(CentralProcessor processor) {
+		oshi.add(processor.toString());
+	}
+
+	private static void printSensors(Sensors sensors) {
+		oshi.add("Sensors: " + sensors.toString());
+	}
+
+}
---
+++ src/test/java/dev/dashaun/service/jsensor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dashaun.service.jsensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
